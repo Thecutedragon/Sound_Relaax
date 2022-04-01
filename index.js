9dda9b38d2eba6e48361eb956c2c3b5f5f5546ae
Max Andell <max.andell@skola.taby.se>
2022-04-01 15:10:27 +0200

    Sound added

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 
-
+//New audio
 const forestAudio = new Audio('forest.wav');
 forestAudio.loop = true;
-//  function () {
-//     document.getElementById("forest").style.color = `rgb(255,255,255)`
-//     forestAudio.play();
-// }
 
+const windAudio = new Audio(`strongWind.wav`);
+windAudio.loop = true;
+
+const rainAudio = new Audio(`rain.wav`);
+rainAudio.loop = true;
+
+const fireAudio = new Audio(`fire.wav`);
+fireAudio.loop = true;
+
+const wavesAudio = new Audio(`waves.wav`);
+wavesAudio.loop = true;
+
+const leafAudio = new Audio(``);
+leafAudio.loop = true;
+// Play function
 const forestPlay = document.getElementById("forest")
 
 forestPlay.addEventListener('click', () => {
     if (forestAudio.paused) return forestAudio.play();
     forestAudio.pause();
 });
+
+const windPlay = document.getElementById("wind")
+
+windPlay.addEventListener(`click`, () =>{
+        if(windAudio.paused) return windAudio.play();
+        windAudio.pause();
+    });
+
+const rainPlay = document.getElementById(`rain`)
+
+rainPlay.addEventListener(`click`, () =>{
+    if(rainAudio.paused) return rainAudio.play();
+    rainAudio.pause();
+})
+
+const firePlay = document.getElementById(`fire`)
+
+firePlay.addEventListener(`click`, () => {
+    if(fireAudio.paused) return fireAudio.play();
+    fireAudio.pause();
+})
+
+const wavesPlay = document.getElementById(`waves`)
+
+wavesPlay.addEventListener(`click`, () => {
+    if(wavesAudio) return wavesAudio.play();
+    wavesAudio.pause();
+})
+
+const leafPlay = document.getElementById(`leavsBlowinginWind`)
+
+leafPlay.addEventListener(`click`, () => {
+    if(leafAudio) return leafAudio.play();
+    leafAudio.pause();
+})
